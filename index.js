28019ac15785a2ace1de85baf33206aff09d44a6
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-03-12 13:56:36 -0600

    dodger moves left and right

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    /* The width of dodger is 40, so stop the leftmost side of dodger at 400 - dodger width(40) = 360 */
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  }
+});
